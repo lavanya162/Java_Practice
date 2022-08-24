832e3ada70b0b4449c47463c25458d2c26e9194f
lavanya162 <107476913+lavanya162@users.noreply.github.com>
2022-08-24 07:54:28 +0530

    Add files via upload

---
+++ Decimal.java
@@ -0,0 +1,10 @@
+class Decimal
+  {
+    public static void main(String args[])
+    {
+      float ft = 10.23F;
+      double d = 2343.43;
+      System.out.println("The decimal value is:"+ft);
+      System.out.println("The decimal value is:"+d);
+    }
+  }
