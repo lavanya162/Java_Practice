3758893b15207d949e90449d5ad3b6877dfbf590
lavanya162 <107476913+lavanya162@users.noreply.github.com>
2022-08-24 07:55:10 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.lang.String;
+
+class First
+  {
+    public static void main (String [] args)
+    {
+      System.out.println ("WELCOME TO FIRST JAVA PROGRAMMING");
+      System.out.println ("WELCOME TO BITLABS");
+    }
+  }
