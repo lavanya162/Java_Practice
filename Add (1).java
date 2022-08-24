8f9c148e8e77c9264a2e9baaa1cf322402b73438
lavanya162 <107476913+lavanya162@users.noreply.github.com>
2022-08-24 07:54:01 +0530

    Add files via upload

---
+++ Add (1).java
@@ -0,0 +1,11 @@
+class Add
+  {
+    public static void main (String [] args)
+    {
+      int a=100, b=200;
+      System.out.println ("The a value is:"+a);
+      System.out.println ("The b value is:"+b);
+      System.out.println ("The addition value is:"+(a+b));
+      
+    }
+  }
